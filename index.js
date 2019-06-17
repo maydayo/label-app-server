3f94126900cbd5c46fd7bd866369a387a2e1dbd8
maydayo <wanwisa.silanan@gmail.com>
2019-06-17 18:04:52 +0700

    mongodb init

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 const port = 3000
+
+mongoose.connect( // Connect to mongodb
+    `mongodb://localhost:27017/label_pic_database`,
+    //`mongodb://mongo:27017/label_pic_database`,
+    { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false }
+  )
 
 app.disable('x-powered-by')
 
